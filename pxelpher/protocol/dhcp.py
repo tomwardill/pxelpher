0d3402b97742ee6a7e46f7cbc3b4b56e74b324ea
Tom Wardill <tom@howrandom.net>
2019-09-12 20:11:03 +0100

    Successfully parse a Discover packet

--- pxelpher/protocol/dhcp.py
+++ pxelpher/protocol/dhcp.py
@@ -1,6 +1,22 @@
 import binascii
+from enum import Enum
 
 from dataclasses import dataclass
+
+
+class PacketMode(Enum):
+
+    DISCOVER = 1
+    OFFER = 1
+    REQUEST = 1
+
+
+class DHCPOption:
+
+    def __init__(self, code, length, value):
+        self.code = code
+        self.length = length
+        self.value = value
 
 
 @dataclass
@@ -17,10 +33,43 @@ class DiscoverPacket:
     SiAddr: bytearray
     GiAddr: bytearray
     CHAddr: bytearray
+    SName: str
+    File: str
+    RawOptions: bytearray
 
     @property
     def is_broadcast(self):
         return self.Flags == 1
+
+    @property
+    def magic_cookie(self):
+        cookie_bytes = self.RawOptions[0:4]
+        return '.'.join(str(int(i)) for i in cookie_bytes)
+
+    @property
+    def Options(self):
+        available_options = []
+        without_magic_cookie = self.RawOptions[4:]
+        i = 0
+        while without_magic_cookie[i] != 255:
+            code = without_magic_cookie[i]
+            i = i + 1
+            length = without_magic_cookie[i]
+            i = i + 1
+            value = without_magic_cookie[i:i+length]
+            i = i + length
+            available_options.append(DHCPOption(
+                code, length, value
+            ))
+        return available_options
+
+    @property
+    def mode(self):
+        for option in self.Options:
+            if option.code == 53:
+                return PacketMode(
+                    int.from_bytes(option.value, byteorder='big'))
+        raise ValueError("No mode Option found")
 
     @property
     def hardware_address(self):
@@ -49,6 +98,9 @@ class DiscoverPacket:
             YiAddr=packet[16:20],
             SiAddr=packet[20:24],
             GiAddr=packet[24:28],
-            CHAddr=packet[28:45]
+            CHAddr=packet[28:44],
+            SName=packet[44:108],
+            File=packet[108:236],
+            RawOptions=packet[236:]
         )
         return discover
