801d6a6acfb2342d892ac195e709669637d7abba
Tom Wardill <tom@howrandom.net>
2019-09-16 07:42:05 +0100

    Get to Request stage. No TFTP Options yet

--- pxelpher/protocol/dhcp.py
+++ pxelpher/protocol/dhcp.py
@@ -21,9 +21,26 @@ class DHCPOption:
     def __repr__(self):
         return "DHCP Option {}: {}".format(self.code, self.value)
 
+    def encoded(self):
+        return '{}{}{}'.format(
+            chr(self.code),
+            chr(self.length),
+            self.value
+        ).encode()
+
+
+def f_i2h(int_to_format):
+    return '0x{:02x}'.format(int_to_format)
+
+
+def ip_to_hex(ip):
+    return bytearray(int(x) for x in ip.split('.'))
+    #return (''.join(chr(int(x)) for x in ip.split('.'))).encode()
+    #return '0x{}'.format(''.join('{:02x}'.format(int(x)) for x in ip.split('.')).upper())
+
 
 @dataclass
-class DiscoverPacket:
+class DHCPPacket:
     Op: int
     HType: int
     HLen: int
@@ -39,6 +56,7 @@ class DiscoverPacket:
     SName: str
     File: str
     RawOptions: bytearray
+    RawPacket: bytearray
 
     def __repr__(self):
         return "{} from {}".format(self.mode, self.hardware_address)
@@ -51,6 +69,10 @@ class DiscoverPacket:
     def magic_cookie(self):
         cookie_bytes = self.RawOptions[0:4]
         return '.'.join(str(int(i)) for i in cookie_bytes)
+
+    @property
+    def raw_magic_cookie(self):
+        return self.RawOptions[0:4]
 
     @property
     def Options(self):
@@ -92,7 +114,7 @@ class DiscoverPacket:
 
     @staticmethod
     def from_network(packet):
-        discover = DiscoverPacket(
+        discover = DHCPPacket(
             Op=packet[0],
             HType=packet[1],
             HLen=packet[2],
@@ -107,6 +129,36 @@ class DiscoverPacket:
             CHAddr=packet[28:44],
             SName=packet[44:108],
             File=packet[108:236],
-            RawOptions=packet[236:]
+            RawOptions=packet[236:],
+            RawPacket=packet
         )
         return discover
+
+    @staticmethod
+    def make_offer(discover_packet):
+        raw_options = discover_packet.raw_magic_cookie
+        offer_option = DHCPOption(53, 1, chr(2))
+        raw_options = raw_options + offer_option.encoded() + b'\xff'
+        offer = DHCPPacket(
+            Op=chr(2).encode(),
+            HType=chr(1).encode(),
+            HLen=chr(6).encode(),
+            Hops=chr(0).encode(),
+            XID=binascii.unhexlify(discover_packet.XID),
+            Secs=b'\x00\x00',
+            Flags=b'\x00\x00',
+            CiAddr=b'\x00\x00\x00\x00',
+            YiAddr=ip_to_hex('192.168.58.2'),
+            SiAddr=ip_to_hex('192.168.58.1'),
+            GiAddr=ip_to_hex('0.0.0.0'),
+            CHAddr=discover_packet.CHAddr,
+            SName=discover_packet.SName,
+            File=discover_packet.File,
+            RawOptions=raw_options,
+            RawPacket='unknown'
+        )
+        return offer
+
+    def make_raw(self):
+        packet = self.Op + self.HType + self.HLen + self.Hops + self.XID + self.Secs + self.Flags + self.CiAddr + self.YiAddr + self.SiAddr + self.GiAddr + self.CHAddr + self.SName + self.File + self.RawOptions
+        return packet
